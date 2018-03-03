4a84c06591d81dc6aeb247953a9c47dbc8f7d232
wmthompson1 <wm.thompson1@gmail.com>
2018-03-02 19:10:58 -0800

    Friday 2/23 synch

--- DEV/20180302/Services/ISurveyQuestionRepository.cs
+++ DEV/20180302/Services/ISurveyQuestionRepository.cs
@@ -1,16 +1,19 @@
 ﻿using AspNetCoreWebAPI.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AspNetCoreWebAPI.Services
 {
     public interface ISurveyQuestionRepository
     {
         // IEnumerable<SurveyQuestionDTO> GetSurveyQuestions();
+        // Case 3
+        IEnumerable<SurveyQuestionDTO> GetSurveyQuestion(int SurveyId);
         // Case 2
-        SurveyQuestionDTO GetSurveyQuestion(int SurveyId);
+        //SurveyQuestionDTO GetSurveyQuestion(int SurveyId);
         // Case 1
         //IEnumerable<SurveyQuestionDTO> GetSurveyQuestion(int SurveyId);
-         //SurveyQuestionDTO GetSurveyQuestion(int SurveyId);
+        //SurveyQuestionDTO GetSurveyQuestion(int SurveyId);
         //void AddSurveyQuestion(SurveyQuestionCreateDTO SurveyQuestion);
         //void UpdateSurveyQuestion(int id, SurveyQuestionUpdateDTO SurveyQuestion);
         //void DeleteSurveyQuestion(SurveyQuestionDTO SurveyQuestion);
--- DEV/20180302/Services/SurveyQuestionSQLRepository.cs
+++ DEV/20180302/Services/SurveyQuestionSQLRepository.cs
@@ -35,24 +35,56 @@ namespace AspNetCoreWebAPI.Services
         //}
 
         // case 2
-        public SurveyQuestionDTO GetSurveyQuestion(int surveyId)
+        //public SurveyQuestionDTO GetSurveyQuestion(int surveyId)
         // case 1
-        //public IEnumerable<SurveyQuestionDTO> GetSurveyQuestion(int surveyId)
+        public IEnumerable<SurveyQuestionDTO> GetSurveyQuestion(int surveyId)
         {
 
+            //// case 5C 3/2/2018 
 
+            var surveyQuestion = _db.SurveyQuestion
+            .FromSql("Select * from test.SurveyQuestion")
+            .Where(p => p.SurveyId.Equals(surveyId));
+
+            foreach (SurveyQuestion s in surveyQuestion)
+            {
+
+
+                //    //var surveyQuestion = _db.SurveyQuestion.Include(d => d.QItem);
+                //    // case 6 https://docs.microsoft.com/en-us/aspnet/core/data/ef-mvc/read-related-data
+
+
+
+                // case 1
+                //_db.Entry(surveyQuestion).Collection(c => QItem).Load();
+
+                // case 2
+                _db.SurveyQuestion
+                    .Include(d => d.QItem)
+                    .FirstOrDefault(p => p.SurveyId.Equals(surveyId))
+                    ;
+
+            }
+            //    .FirstOrDefault(p => p.SurveyId.Equals(surveyId));
+
+            //// case 5B 3/2/2018 - ?
+            //var surveyQuestion = _db.SurveyQuestion
+            //    .Select(p => p.SurveyId.Equals(surveyId));
+
+            // case 5B 3/2/2018 - **BUT ONLY ONE ROW **
+            //var surveyQuestion = _db.SurveyQuestion
+            //    .FirstOrDefault(p => p.SurveyId.Equals(surveyId));
+
+
+            // case 5A 3/2/2018 works with just dto case 2
+            //var surveyQuestion = _db.SurveyQuestion
+            //    .FirstOrDefault(p => p.SurveyId == 1);
 
             //// case 7
             //var surveyQuestion = _db.SurveyQuestion.Select<IEnumerable<SurveyQuestionDTO>>(p => p.SurveyId.Equals(surveyId));
 
             // case 6
             //var surveyQuestion = _db.SurveyQuestion.Select(p => p.SurveyId.Equals(surveyId));
-
-
-
-            // case 5
-            var surveyQuestion = _db.SurveyQuestion
-                .FirstOrDefault(p => p.SurveyId == 1);
 
 
             // case 1
@@ -75,13 +107,20 @@ namespace AspNetCoreWebAPI.Services
             //                .ThenInclude(c => c.Category)
             //            .FirstOrDefault(p => p.Id == 1));
 
-            // case 2
+            // case 3
+            // ERROR '...but is defined in the model as a collection navigation property'
+            //_db.Entry(surveyQuestion).Reference(c => c.QItem).Load();
+
+            // case 2 ** works 3/2/18 **
             //_db.Entry(surveyQuestion).Collection<QItem>(c => c.QItem).Load();
             // case 1
-            _db.Entry(surveyQuestion).Collection(c => c.QItem).Load();
+            //_db.Entry(surveyQuestion).Collection(c => c.QItem).Load();
 
             // case 2
-            var surveyQuestionDTO = Mapper.Map<SurveyQuestionDTO>(surveyQuestion);
+            var DTOs = Mapper.Map<IEnumerable<SurveyQuestionDTO>>(surveyQuestion);
+
+            // case 2
+            //var DTOs = Mapper.Map<SurveyQuestionDTO>(surveyQuestion);
 
             // case 1
             //var surveyQuestionDTO = Mapper.Map<IEnumerable<SurveyQuestionDTO>>(surveyQuestion);
@@ -100,11 +139,14 @@ namespace AspNetCoreWebAPI.Services
 
             }
 
+            // case 3
+            // return DTOs
+
             // case 2
-            // ??
+            // ?
 
             // case 1
-            return surveyQuestionDTO;
+            return DTOs;
            
         }
 
