96d6713d067fbd9f0d7b95522b863cd576806941
wmthompson1 <wm.thompson1@gmail.com>
2018-03-20 10:52:49 -0700

    Tues 3/20 synch

---
+++ DEV/20180302/Entities/_bak_clean_SqlDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreWebAPI.Entities
+{
+
+    public class SqlDbContext : DbContext
+    {
+
+        public DbSet<Survey> Survey { get; set; }
+        public DbSet<SurveyQuestionDetail> SurveyQuestionDetail { get; set; }
+        public DbSet<SurveyQuestion> SurveyQuestion { get; set; }
+        public DbSet<QItem> QItem { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SurveyQuestionDetail>()
+                .HasKey(t => new { t.SurveyId, t.PageId, t.QuestionId });
+
+
+            modelBuilder.Entity<SurveyQuestion>()
+                .HasKey(t => new { t.QuestionId });
+
+
+            //**MANY-TO-MANY pattern; SurveyQuestion ->1 -- >8 QItem
+
+            modelBuilder.Entity<QItem>(entity =>
+            {
+
+                entity.HasOne(d => d.SurveyQuestion)
+                    .WithMany(p => p.QItem)
+                    .HasForeignKey(d => d.QuestionId);
+            });
+
+
+        }
+
+        public SqlDbContext(DbContextOptions<SqlDbContext> options)
+        : base(options)
+        {
+        }
+
+
+    }
+}
--- f0.bat
+++ f0.bat
@@ -1,6 +1,6 @@
 
 git add -A
-git commit -m "Friday 2/23 synch"
+git commit -m "Tues 3/20 synch"
 :: git push -u origin master
 :: git push https://github.com/wmthompson1/seed3.git 
 :: to force use -f
